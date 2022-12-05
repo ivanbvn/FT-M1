088af970b4727e53822fda08a85765929ce34484
bilvinas.ivan@gmail.com <bilvinas.ivan@gmail.com>
2022-12-05 15:24:51 -0300

    01-IntroToCs Homework - terminada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,23 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let decimalNumber = 0
+   for (let i = 0; i < num.length; i++) {
+      decimalNumber = decimalNumber + Number(num[num.length - (i + 1)]) * Math.pow(2, i)
+   }
+   return decimalNumber
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let result = num
+   let binaryNumbers = []
+   while (result != 0) {
+      binaryNumbers.push(result % 2)
+      result = Math.floor(result / 2)
+      console.log(result)
+   }
+   return binaryNumbers.reverse().join('')
+}
 
 module.exports = {
    BinarioADecimal,
